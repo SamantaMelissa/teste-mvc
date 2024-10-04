4bbe7b63cb94d42de072e72eaf9d73ef703b1abf
Samanta Melissa <samanta.melissa13@gmail.com>
2024-10-04 16:56:47 -0300

    day 01

---
+++ Bibliotec/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bibliotec.Controllers
+{
+    // [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /Login/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Login/Acessar
+        public IActionResult Acessar()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
